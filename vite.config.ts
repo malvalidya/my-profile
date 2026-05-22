7587fa6591acd88547f2c514bbcc12daf19e597c
malvalidya <malva.students.2505336@gmail.com>
2026-05-22 10:38:17 +0700

    tambah workflow deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-/// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/my-profile/' : '/',
   test: {
-    globals: true,
     environment: 'jsdom',
-    setupFiles: ['src/test/setup.ts'],
+    globals: true,
   },
 })
